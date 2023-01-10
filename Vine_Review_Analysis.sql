481cd28cd259fb692c12ea541d99c5659937384a
cmason1996 <astronomy747@yahoo.com>
2023-01-09 16:50:59 -0800

    Adding initial SQL file to repo

---
+++ Vine_Review_Analysis.sql
@@ -0,0 +1,12 @@
+-- vine table
+CREATE TABLE vine_table (
+  review_id TEXT PRIMARY KEY,
+  star_rating INTEGER,
+  helpful_votes INTEGER,
+  total_votes INTEGER,
+  vine TEXT,
+  verified_purchase TEXT
+);
+
+SELECT * FROM vine_table;
+
